7c7423813d16d31d38cecb15f342fed80d4745b1
winston.grant <winston.grant@revature.net>
2021-07-13 18:19:54 -0700

    added an update rating test

--- P2Folder/ApiGameBook/BusinessLayer/GameMethods.cs
+++ P2Folder/ApiGameBook/BusinessLayer/GameMethods.cs
@@ -15,14 +15,18 @@ namespace BusinessLayer
         {
             _context = context;
         }
-
+		/// <summary>
+		/// This will either do the initial rating or update a rating
+		/// </summary>
+		/// <param name="user">Tracks user who rates</param>
+		/// <param name="game">Tracks the game to be rated</param>
+		/// <param name="rating">Tracks the rating for the game by user</param>
+		/// <returns>Only returns false if something terrible happens</returns>
         public bool RateGame(User user, Game game, int rating)
 		{
 			try
 			{
-
-
-
+				//fills the object rate with everything it needs
 				Rating rate = new Rating()
 				{
 					UserId = user.UserId,
@@ -31,12 +35,12 @@ namespace BusinessLayer
 					Game = game,
 					User = user
 				};
-
+				//checks if this user already rated said game
 				Rating temp = (from x in _context.Ratings
 							   where x.GameId == game.GameId &&
 							   x.UserId == user.UserId
 							   select x).FirstOrDefault();
-
+				//if the user hasn't it will create a new rating
 				if (temp == null)
 				{
 					_context.Ratings.Add(rate);
@@ -45,12 +49,14 @@ namespace BusinessLayer
 				}
 				else
 				{
-
-					_context.Ratings.Update(rate);
+					temp.Rating1 = rating;
+					//else it will updayte the rating
+					_context.Ratings.Update(temp);
 					_context.SaveChanges();
 					return true;
 				}
 			}
+			//if for some reason the crash happens it wil just return false
 			catch(SystemException)
 			{
 				return false;
