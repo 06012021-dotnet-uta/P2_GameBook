e827b3f0e9e1af2733f30f3049a1a5352503cb91
winston.grant <winston.grant@revature.net>
2021-07-16 17:04:34 -0700

    unable to correct DB problems the code should work just fine in fact there is a whole portion for a duplicate checker just in case but Db needs seeding

--- P2Folder/ApiGameBook/SeedConsole/Program.cs
+++ P2Folder/ApiGameBook/SeedConsole/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using BusinessLayer;
 
 namespace SeedConsole
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
+            PopulateDBRealQuickMethod populateDB = new();
+
             Console.WriteLine("Hello World!");
+
+            populateDB.PopulateThatDb();
         }
     }
 }
