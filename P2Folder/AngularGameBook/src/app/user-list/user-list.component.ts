ce49b03ac11718769e9b3c120f7dc0c903bda799
EthanBaker3023 <ethan.baker@revature.net>
2021-07-17 15:23:57 -0600

    display gamebook users

--- P2Folder/AngularGameBook/src/app/user-list/user-list.component.ts
+++ P2Folder/AngularGameBook/src/app/user-list/user-list.component.ts
@@ -1,18 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { USERS } from '../mock-userList';
+
+export class User {
+  constructor(
+    public userId: number,
+    public username: string,
+    public password: string,
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+  ) {
+  }
+}
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
+
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[] | undefined;
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
-  users = USERS;
+  getUsers(){
+    this.httpClient.get<any>('https://localhost:5001/api/User').subscribe(
+      response => {
+        console.log(response);
+        this.users = response;
+      }
+    )
+  }
 
 }
